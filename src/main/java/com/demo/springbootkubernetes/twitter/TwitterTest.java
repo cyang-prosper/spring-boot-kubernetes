dc1aff3882e9be03e6082289caf4695f8374b5e3
cyang-prosper <cyang@prosper.com>
2017-08-27 23:20:40 -0700

    Added src directory.

---
+++ src/main/java/com/demo/springbootkubernetes/twitter/TwitterTest.java
@@ -0,0 +1,35 @@
+package com.demo.springbootkubernetes.twitter;
+
+import java.io.IOException;
+
+import twitter4j.StallWarning;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.TwitterException;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+
+public class TwitterTest {
+
+	public static void main(String[] args) throws TwitterException, IOException{
+	    StatusListener listener = new StatusListener(){
+	        public void onStatus(Status status) {
+	            System.out.println(status.getUser().getName() + " : " + status.getText());
+	        }
+	        public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {}
+	        public void onTrackLimitationNotice(int numberOfLimitedStatuses) {}
+	        public void onException(Exception ex) {
+	            ex.printStackTrace();
+	        }
+			public void onScrubGeo(long userId, long upToStatusId) {}
+			public void onStallWarning(StallWarning warning) {}
+	        
+	    };
+	    TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+	    twitterStream.addListener(listener);
+	    // sample() method internally creates a thread which manipulates TwitterStream and calls these adequate listener methods continuously.
+	    twitterStream.sample();
+	}
+	
+}
